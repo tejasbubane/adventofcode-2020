ae6804294ada6bb0437505978e233e8ca0b9140f
Tejas Bubane <tejasbubane@gmail.com>
2020-12-01 17:18:13 +0530

    Solve day-1 part-1

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,13 @@
-pub fn run(s: u32) -> u32 {
-    s * 2
+const SUM: u32 = 2020;
+
+pub fn run(entries: Vec<u32>) -> Option<u32> {
+    for (index, entry) in entries.iter().enumerate() {
+        let rest = &entries[(index + 1)..];
+
+        match rest.iter().find(|&x| x + entry == SUM) {
+            Some(&other) => return Some(entry * other),
+            None => continue,
+        }
+    }
+    None
 }
