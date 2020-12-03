3dae4727d3a2e1477a88d79b1d23ff472d736bdb
Tejas Bubane <tejasbubane@gmail.com>
2020-12-03 18:09:29 +0530

    Complete day-2 part-2

--- src/day2.rs
+++ src/day2.rs
@@ -4,32 +4,51 @@ use regex::Regex;
 
 lazy_static! {
     static ref REGEXP: Regex =
-        Regex::new(r"(?P<min>\d+)-(?P<max>\d+) (?P<letter>[a-z]): (?P<password>[a-z]+)").unwrap();
+        Regex::new(r"(?P<x>\d+)-(?P<y>\d+) (?P<letter>[a-z]): (?P<password>[a-z]+)").unwrap();
 }
 
-pub fn run(filename: &str) -> usize {
+pub fn run_1(filename: &str) -> usize {
     match read_lines(filename) {
         Ok(lines) => lines
             .map(|line| line.unwrap())
-            .filter(|line| matches(&line))
+            .filter(|line| check_count(&line))
             .count(),
         Err(_) => 0,
     }
 }
 
-fn matches(password_req: &str) -> bool {
+pub fn run_2(filename: &str) -> usize {
+    match read_lines(filename) {
+        Ok(lines) => lines
+            .map(|line| line.unwrap())
+            .filter(|line| check_position(&line))
+            .count(),
+        Err(_) => 0,
+    }
+}
+
+fn check_count(password_req: &str) -> bool {
     let (min, max, letter, password) = parse(password_req);
     let letter_count = password.chars().filter(|&p| p == letter).count();
 
     min <= letter_count && letter_count <= max
 }
 
+fn check_position(password_req: &str) -> bool {
+    let (first, second, letter, password) = parse(password_req);
+    let chars: Vec<char> = password.chars().collect();
+    let first_char = chars[first - 1];
+    let second_char = chars[second - 1];
+
+    first_char == letter && second_char != letter || first_char != letter && second_char == letter
+}
+
 fn parse(password_req: &str) -> (usize, usize, char, &str) {
     let caps = REGEXP.captures(password_req).unwrap();
-    let min: usize = caps.name("min").unwrap().as_str().parse().unwrap();
-    let max: usize = caps.name("max").unwrap().as_str().parse().unwrap();
+    let x: usize = caps.name("x").unwrap().as_str().parse().unwrap();
+    let y: usize = caps.name("y").unwrap().as_str().parse().unwrap();
     let letter: char = caps.name("letter").unwrap().as_str().parse().unwrap();
     let password = caps.name("password").unwrap().as_str();
 
-    (min, max, letter, password)
+    (x, y, letter, &password)
 }
--- tests/day2_test.rs
+++ tests/day2_test.rs
@@ -1,14 +1,24 @@
 #[cfg(test)]
 mod day2_test {
-    use adventofcode_2020::day2::run;
+    use adventofcode_2020::day2::{run_1, run_2};
 
     #[test]
     fn part1_works_for_sample_input() {
-        assert_eq!(run("tests/inputs/day2_sample.txt"), 2);
+        assert_eq!(run_1("tests/inputs/day2_sample.txt"), 2);
     }
 
     #[test]
     fn part1_works_for_puzzle_input() {
-        assert_eq!(run("tests/inputs/day2_puzzle.txt"), 614);
+        assert_eq!(run_1("tests/inputs/day2_puzzle.txt"), 614);
+    }
+
+    #[test]
+    fn part2_works_for_sample_input() {
+        assert_eq!(run_2("tests/inputs/day2_sample.txt"), 1);
+    }
+
+    #[test]
+    fn part2_works_for_puzzle_input() {
+        assert_eq!(run_2("tests/inputs/day2_puzzle.txt"), 354);
     }
 }
