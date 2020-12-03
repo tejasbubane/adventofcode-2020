61cf20f4466147eede0104b3177a8f58c03cd47c
Tejas Bubane <tejasbubane@gmail.com>
2020-12-03 21:08:17 +0530

    Solve day-3 part-2

--- src/day3.rs
+++ src/day3.rs
@@ -1,11 +1,12 @@
 use crate::utils::read_lines;
 use grid::*;
 
-pub fn run(filename: &str) -> usize {
+// Part 1
+pub fn slope_for(filename: &str, right_step: usize, down_step: usize) -> usize {
     let mut terrain = grid![];
     let mut tree_count = 0;
-    let mut row = 1;
-    let mut col = 3;
+    let mut row = down_step;
+    let mut col = right_step;
 
     match read_lines(filename) {
         Ok(lines) => lines.for_each(|line| terrain.push_row(construct_grid_line(&line.unwrap()))),
@@ -19,10 +20,15 @@ pub fn run(filename: &str) -> usize {
         if *terrain.get(row, col).unwrap() {
             tree_count = tree_count + 1
         }
-        row = row + 1;
-        col = col + 3;
+        row = row + down_step;
+        col = col + right_step;
     }
     tree_count
+}
+
+// Part 2
+pub fn run(filename: &str, paths: &[(usize, usize)]) -> usize {
+    paths.iter().map(|(right_step, down_step)| slope_for(filename, *right_step, *down_step)).product()
 }
 
 fn construct_grid_line(line: &str) -> Vec<bool> {
--- tests/day3_test.rs
+++ tests/day3_test.rs
@@ -1,14 +1,26 @@
 #[cfg(test)]
 mod day3_test {
-    use adventofcode_2020::day3::run;
+    use adventofcode_2020::day3::{slope_for, run};
+
+    const STEPS: [(usize, usize); 5] = [(1,1), (3,1), (5,1), (7,1), (1,2)];
 
     #[test]
     fn part1_works_for_sample_input() {
-        assert_eq!(run("tests/inputs/day3_sample.txt"), 7);
+        assert_eq!(slope_for("tests/inputs/day3_sample.txt", 3, 1), 7);
     }
 
     #[test]
     fn part1_works_for_puzzle_input() {
-        assert_eq!(run("tests/inputs/day3_puzzle.txt"), 173);
+        assert_eq!(slope_for("tests/inputs/day3_puzzle.txt", 3, 1), 173);
+    }
+
+    #[test]
+    fn part2_works_for_sample_input() {
+        assert_eq!(run("tests/inputs/day3_sample.txt", &STEPS), 336);
+    }
+
+    #[test]
+    fn part2_works_for_puzzle_input() {
+        assert_eq!(run("tests/inputs/day3_puzzle.txt", &STEPS), 4385176320);
     }
 }
