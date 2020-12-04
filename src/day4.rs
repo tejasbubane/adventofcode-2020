1f5be1c94b5184bc8b5cd94debbd5c690d24ce51
Tejas Bubane <tejasbubane@gmail.com>
2020-12-05 02:12:35 +0530

    Solve day-4 part-2

--- src/day4.rs
+++ src/day4.rs
@@ -1,18 +1,106 @@
-use crate::utils::read_to_string;
-use std::collections::HashSet;
+#![allow(clippy::ptr_arg)]
 
-pub fn run(filename: &str) -> usize {
-    let contents = read_to_string(filename).expect("Error reading file.");
-    let req_fields: HashSet<&str> = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+use crate::utils::read_to_string;
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
+
+pub fn run_1(filename: &str) -> usize {
+    let filestr = read_to_string(filename).expect("Error reading file.");
+    passports_with_required_fields(&filestr).iter().count()
+}
+
+pub fn run_2(filename: &str) -> usize {
+    let filestr = read_to_string(filename).expect("Error reading file.");
+    passports_with_required_fields(&filestr)
         .iter()
-        .cloned()
+        .filter(|&p| is_valid(p))
+        .count()
+}
+
+// Validations
+
+fn is_valid(passport: &HashMap<String, String>) -> bool {
+    passport.get("byr").map_or_else(|| false, valid_byr)
+        && passport.get("iyr").map_or_else(|| false, valid_iyr)
+        && passport.get("eyr").map_or_else(|| false, valid_eyr)
+        && passport.get("hgt").map_or_else(|| false, valid_hgt)
+        && passport.get("hcl").map_or_else(|| false, valid_hcl)
+        && passport.get("ecl").map_or_else(|| false, valid_ecl)
+        && passport.get("pid").map_or_else(|| false, valid_pid)
+}
+
+fn valid_byr(birth_year: &String) -> bool {
+    match birth_year.parse::<usize>() {
+        Ok(year) => year >= 1920 && year <= 2002,
+        Err(_) => false,
+    }
+}
+
+fn valid_iyr(issue_year: &String) -> bool {
+    match issue_year.parse::<usize>() {
+        Ok(year) => year >= 2010 && year <= 2020,
+        Err(_) => false,
+    }
+}
+
+fn valid_eyr(issue_year: &String) -> bool {
+    match issue_year.parse::<usize>() {
+        Ok(year) => year >= 2020 && year <= 2030,
+        Err(_) => false,
+    }
+}
+
+fn valid_hgt(height_str: &String) -> bool {
+    let len = height_str.len();
+    let height = &height_str[..len - 2];
+    let unit = &height_str[(len - 2)..];
+
+    match (height.parse::<usize>(), unit) {
+        (Ok(l), "cm") => l >= 150 && l <= 193,
+        (Ok(l), "in") => l >= 59 && l <= 76,
+        _ => false,
+    }
+}
+
+lazy_static! {
+    static ref HAIR_COLOR_REGEX: Regex = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
+}
+
+fn valid_hcl(hair_color: &String) -> bool {
+    HAIR_COLOR_REGEX.is_match(hair_color)
+}
+
+const EYE_COLORS: [&str; 7] = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+
+fn valid_ecl(eye_color: &String) -> bool {
+    EYE_COLORS.iter().any(|&c| c == eye_color)
+}
+
+lazy_static! {
+    static ref PASSPORT_ID_REGEX: Regex = Regex::new(r"^[0-9]{9}$").unwrap();
+}
+
+fn valid_pid(passport_id: &String) -> bool {
+    PASSPORT_ID_REGEX.is_match(passport_id)
+}
+
+// Parsing & helper functions
+
+fn passports_with_required_fields(filestr: &str) -> Vec<HashMap<String, String>> {
+    let req_fields: HashSet<String> = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+        .iter()
+        .map(|p| p.to_string())
         .collect();
 
-    passport_strings(&contents)
+    passport_strings(filestr)
         .iter()
-        .map(|s| passport(s))
-        .filter(|p| req_fields.is_subset(p))
-        .count()
+        .map(passport)
+        .filter(|p| {
+            let keys: HashSet<String> = p.keys().map(|p| p.to_string()).collect();
+            req_fields.is_subset(&keys)
+        })
+        .collect()
 }
 
 fn passport_strings(filecontents: &str) -> Vec<String> {
@@ -22,11 +110,13 @@ fn passport_strings(filecontents: &str) -> Vec<String> {
         .collect()
 }
 
-fn passport(passport_string: &str) -> HashSet<&str> {
-    let mut keys = HashSet::new();
+fn passport(passport_string: &String) -> HashMap<String, String> {
+    let mut map = HashMap::new();
     passport_string.split(' ').for_each(|p| {
         let split: Vec<&str> = p.split(':').collect();
-        keys.insert(split[0]);
+        if split.len() > 1 {
+            map.insert(split[0].to_string(), split[1].to_string());
+        }
     });
-    keys
+    map
 }
--- tests/day4_test.rs
+++ tests/day4_test.rs
@@ -1,14 +1,24 @@
 #[cfg(test)]
 mod day4_test {
-    use adventofcode_2020::day4::run;
+    use adventofcode_2020::day4::{run_1, run_2};
 
     #[test]
     fn part1_works_for_sample_input() {
-        assert_eq!(run("tests/inputs/day4_sample.txt"), 2);
+        assert_eq!(run_1("tests/inputs/day4_sample.txt"), 2);
     }
 
     #[test]
     fn part1_works_for_puzzle_input() {
-        assert_eq!(run("tests/inputs/day4_puzzle.txt"), 216);
+        assert_eq!(run_1("tests/inputs/day4_puzzle.txt"), 216);
+    }
+
+    #[test]
+    fn part2_works_for_sample_input() {
+        assert_eq!(run_2("tests/inputs/day4_part2_sample.txt"), 4);
+    }
+
+    #[test]
+    fn part2_works_for_puzzle_input() {
+        assert_eq!(run_2("tests/inputs/day4_puzzle.txt"), 150);
     }
 }
