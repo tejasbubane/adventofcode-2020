9c6f3549de899f1e6f8b73b1b07f5f4ac638742e
Tejas Bubane <tejasbubane@gmail.com>
2020-12-05 13:50:31 +0530

    Solve day-4 part-2

--- src/day5.rs
+++ src/day5.rs
@@ -1,6 +1,6 @@
 use crate::utils::read_lines;
 
-pub fn run(filename: &str) -> Vec<usize> {
+pub fn run_1(filename: &str) -> Vec<usize> {
     match read_lines(filename) {
         Ok(lines) => lines.map(|line| calc_seat_number(&line.unwrap())).collect(),
         Err(_) => {
@@ -10,7 +10,25 @@ pub fn run(filename: &str) -> Vec<usize> {
     }
 }
 
+pub fn run_2(filename: &str) -> usize {
+    let all_seats = run_1(&filename);
+    match (all_seats.iter().min(), all_seats.iter().max()) {
+        (Some(&min), Some(&max)) => ((min + 1)..max)
+            .find(|&s| {
+                !all_seats.contains(&s)
+                    && all_seats.contains(&(s - 1))
+                    && all_seats.contains(&(s + 1))
+            })
+            .unwrap(),
+        _ => 0,
+    }
+}
+
 fn calc_seat_number(seat_str: &str) -> usize {
-    let seat_str = seat_str.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1");
+    let seat_str = seat_str
+        .replace("F", "0")
+        .replace("B", "1")
+        .replace("L", "0")
+        .replace("R", "1");
     usize::from_str_radix(&seat_str, 2).unwrap()
 }
--- tests/day5_test.rs
+++ tests/day5_test.rs
@@ -1,14 +1,27 @@
 #[cfg(test)]
 mod day5_test {
-    use adventofcode_2020::day5::run;
+    use adventofcode_2020::day5::{run_1, run_2};
 
     #[test]
     fn part1_works_for_sample_input() {
-        assert_eq!(run("tests/inputs/day5_sample.txt"), vec![357, 567, 119, 820]);
+        assert_eq!(
+            run_1("tests/inputs/day5_sample.txt"),
+            vec![357, 567, 119, 820]
+        );
     }
 
     #[test]
     fn part1_works_for_puzzle_input() {
-        assert_eq!(*run("tests/inputs/day5_puzzle.txt").iter().max().unwrap(), 828);
+        assert_eq!(
+            *run_1("tests/inputs/day5_puzzle.txt").iter().max().unwrap(),
+            828
+        );
+    }
+
+    // No sample input for part 2
+
+    #[test]
+    fn part2_works_for_puzzle_input() {
+        assert_eq!(run_2("tests/inputs/day5_puzzle.txt"), 565);
     }
 }
