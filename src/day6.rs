5b5372c9878e5d5c48390ceccc8088e8bd87d537
Tejas Bubane <tejasbubane@gmail.com>
2020-12-07 17:49:29 +0530

    Solve day-6 part-2

--- src/day6.rs
+++ src/day6.rs
@@ -1,8 +1,10 @@
 use crate::utils::read_to_string;
 use std::collections::HashSet;
 
-pub fn run(filename: &str) -> Vec<usize> {
-    grouped_answers(filename)
+// Part 1
+// Questions "anyone" answered yes
+pub fn run_1(filename: &str) -> Vec<usize> {
+    all_answers(filename)
         .iter()
         .map(|ans| {
             let mut set = HashSet::new();
@@ -14,10 +16,37 @@ pub fn run(filename: &str) -> Vec<usize> {
         .collect()
 }
 
-fn grouped_answers(filename: &str) -> Vec<String> {
+fn all_answers(filename: &str) -> Vec<String> {
     read_to_string(filename)
         .expect("Error reading the file.")
         .split("\n\n")
         .map(|s| s.replace("\n", ""))
+        .collect()
+}
+
+// Part 2
+// Questions "everyone" answered yes
+pub fn run_2(filename: &str) -> Vec<usize> {
+    common_answers(filename).iter().map(|x| x.len()).collect()
+}
+
+fn common_answers(filename: &str) -> Vec<HashSet<char>> {
+    let input = read_to_string(filename).expect("Error reading the file.");
+    let groups: Vec<&str> = input.split("\n\n").collect();
+
+    groups
+        .iter()
+        .map(|group| {
+            let answers: Vec<&str> = group.split('\n').collect();
+            let mut set: HashSet<char> = answers[0].chars().collect();
+
+            answers[1..].iter().for_each(|a| {
+                let aset: HashSet<char> = a.chars().collect();
+                if !aset.is_empty() {
+                    set = set.intersection(&aset).copied().collect::<HashSet<char>>();
+                }
+            });
+            set
+        })
         .collect()
 }
--- tests/day6_test.rs
+++ tests/day6_test.rs
@@ -1,17 +1,30 @@
 #[cfg(test)]
 mod day6_test {
-    use adventofcode_2020::day6::run;
+    use adventofcode_2020::day6::{run_1, run_2};
 
     #[test]
     fn part1_works_for_sample_input() {
-        assert_eq!(run("tests/inputs/day6_sample.txt"), &[3, 3, 3, 1, 1]);
+        assert_eq!(run_1("tests/inputs/day6_sample.txt"), &[3, 3, 3, 1, 1]);
     }
 
     #[test]
     fn part1_works_for_puzzle_input() {
         assert_eq!(
-            run("tests/inputs/day6_puzzle.txt").iter().sum::<usize>(),
+            run_1("tests/inputs/day6_puzzle.txt").iter().sum::<usize>(),
             6782
+        );
+    }
+
+    #[test]
+    fn part2_works_for_sample_input() {
+        assert_eq!(run_2("tests/inputs/day6_sample.txt"), &[3, 0, 1, 1, 1]);
+    }
+
+    #[test]
+    fn part2_works_for_puzzle_input() {
+        assert_eq!(
+            run_2("tests/inputs/day6_puzzle.txt").iter().sum::<usize>(),
+            3596
         );
     }
 }
