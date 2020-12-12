fb977af7b518563ff98c762e342b1c216dae53a6
Tejas Bubane <tejasbubane@gmail.com>
2020-12-13 00:54:13 +0530

    Solve day-7 part-2

--- src/day7.rs
+++ src/day7.rs
@@ -7,16 +7,18 @@ type Capacity = usize;
 type Containers = Vec<(Color, Capacity)>;
 type RuleMap = HashMap<Color, Containers>; // Allowed containers for the color with capacity for each
 
-pub fn run(filename: &str) -> usize {
+// Part 1
+
+pub fn run_1(filename: &str) -> usize {
     match read_lines(filename) {
         Ok(lines) => {
             let color = "shiny gold";
             let mut rules = HashMap::new();
-            lines.for_each(|line| parse_rule(&mut rules, &line.unwrap()));
+            lines.for_each(|line| parse_rule(&mut rules, &line.unwrap(), true));
 
             // Use Set to remove duplicate containers in recursion path
             let mut allowed_containers: HashSet<Color> = HashSet::new();
-            recurse(color, &rules, &mut allowed_containers);
+            recurse_1(color, &rules, &mut allowed_containers);
             allowed_containers.len() - 1 // remove shiny-gold itself
         }
         Err(_) => {
@@ -26,7 +28,11 @@ pub fn run(filename: &str) -> usize {
     }
 }
 
-fn parse_rule(rules: &mut RuleMap, line: &str) {
+// `inverse`  argument is used to create inverse map of containers.
+// For example: "dark orange bags contain 3 bright white bags, 4 muted yellow bags."
+// inverse = false : { "dark_orange" => [("bright white", 3), ("muted yellow", 4)] }
+// inverse = true : { "bright white" => [("dark orange", 3)], "muted yellow" => ["muted yellow", 4] }
+fn parse_rule(rules: &mut RuleMap, line: &str, inverse: bool) {
     let line_parts: Vec<&str> = line.split(" bags contain ").collect();
     let container = line_parts[0];
 
@@ -36,19 +42,51 @@ fn parse_rule(rules: &mut RuleMap, line: &str) {
         if let Ok(count) = words[0].parse::<usize>() {
             let color = format!("{} {}", words[1], words[2]);
             let init_vec = vec![];
-            let mut containers: Containers = rules.get(&color).unwrap_or(&init_vec).clone();
-            containers.push((container.to_string(), count));
-            rules.insert(color, containers);
+            if inverse {
+                let mut inners: Containers = rules.get(&color).unwrap_or(&init_vec).clone();
+                inners.push((container.to_string(), count));
+                rules.insert(color, inners);
+            } else {
+                let mut inners: Containers = rules.get(container).unwrap_or(&init_vec).clone();
+                inners.push((color, count));
+                rules.insert(container.to_string(), inners);
+            }
         };
     })
 }
 
-fn recurse(color: &str, rules: &RuleMap, acc: &mut HashSet<Color>) {
+fn recurse_1(color: &str, rules: &RuleMap, acc: &mut HashSet<Color>) {
     acc.insert(color.to_string());
 
     rules
         .get(color)
         .unwrap_or(&vec![])
         .iter()
-        .for_each(|c| recurse(&c.0, rules, acc));
+        .for_each(|c| recurse_1(&c.0, rules, acc));
+}
+
+// Part 2
+
+pub fn run_2(filename: &str) -> usize {
+    match read_lines(filename) {
+        Ok(lines) => {
+            let color = "shiny gold";
+            let mut rules = HashMap::new();
+            lines.for_each(|line| parse_rule(&mut rules, &line.unwrap(), false));
+            recurse_2(color, &rules)
+        }
+        Err(_) => {
+            println!("Error reading file {}", filename);
+            0
+        }
+    }
+}
+
+fn recurse_2(color: &str, rules: &RuleMap) -> usize {
+    rules
+        .get(color)
+        .unwrap_or(&vec![])
+        .iter()
+        .map(|c| c.1 + c.1 * recurse_2(&c.0, rules))
+        .sum()
 }
--- tests/day7_test.rs
+++ tests/day7_test.rs
@@ -1,14 +1,29 @@
 #[cfg(test)]
 mod day7_test {
-    use adventofcode_2020::day7::run;
+    use adventofcode_2020::day7::{run_1, run_2};
 
     #[test]
     fn part1_works_for_sample_input() {
-        assert_eq!(run("tests/inputs/day7_sample.txt"), 4);
+        assert_eq!(run_1("tests/inputs/day7_sample.txt"), 4);
     }
 
     #[test]
     fn part1_works_for_puzzle_input() {
-        assert_eq!(run("tests/inputs/day7_puzzle.txt"), 248);
+        assert_eq!(run_1("tests/inputs/day7_puzzle.txt"), 248);
+    }
+
+    #[test]
+    fn part2_works_for_sample_input() {
+        assert_eq!(run_2("tests/inputs/day7_sample.txt"), 32);
+    }
+
+    #[test]
+    fn part2_works_for_second_sample_input() {
+        assert_eq!(run_2("tests/inputs/day7_sample2.txt"), 126);
+    }
+
+    #[test]
+    fn part2_works_for_puzzle_input() {
+        assert_eq!(run_2("tests/inputs/day7_puzzle.txt"), 57281);
     }
 }
