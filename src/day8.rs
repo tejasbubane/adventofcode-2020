624b34a0c4a776f9843534ae7faadd415b241502
Tejas Bubane <tejasbubane@gmail.com>
2020-12-13 03:33:54 +0530

    Solve day-8 part-2

--- src/day8.rs
+++ src/day8.rs
@@ -1,42 +1,91 @@
 use crate::utils::read_lines;
 use std::collections::HashSet;
 
+#[derive(Clone, Debug)]
 struct Instruction {
     operation: String,
     argument: isize,
 }
 
-pub fn run(filename: &str) -> isize {
+pub fn run_1(filename: &str) -> isize {
     match read_lines(filename) {
         Ok(lines) => {
             let instructions: Vec<Instruction> = lines.map(|line| parse(&line.unwrap())).collect();
-            let mut executed_instructions: HashSet<isize> = HashSet::new();
-            let mut pointer: isize = 0;
-            let mut accumulator: isize = 0;
-
-            while !executed_instructions.contains(&pointer) {
-                executed_instructions.insert(pointer);
-
-                let instruction = &instructions[pointer as usize];
-                match &instruction.operation[..] {
-                    "acc" => {
-                        accumulator += instruction.argument;
-                    }
-                    "jmp" => {
-                        pointer += instruction.argument;
-                        continue;
-                    }
-                    _ => {}
-                };
-                pointer += 1;
-            }
-            accumulator
+            has_loop(&instructions).1 // assuming this always has loop as per inputs - return acc directly
         }
         Err(_) => {
             println!("Error reading file {}", filename);
             0
         }
     }
+}
+
+pub fn run_2(filename: &str) -> isize {
+    let mut acc = 0;
+
+    match read_lines(filename) {
+        Ok(lines) => {
+            let instructions: Vec<Instruction> = lines.map(|line| parse(&line.unwrap())).collect();
+
+            // Iterate over all instructions and try replacing each jmp and nop to see if the code halts
+            for (i, ins) in instructions.iter().enumerate() {
+                if ins.operation == "acc" {
+                    continue;
+                }
+
+                let new_ins = match &ins.operation[..] {
+                    "jmp" => Instruction {
+                        operation: "nop".to_string(),
+                        argument: ins.argument,
+                    },
+                    "nop" => Instruction {
+                        operation: "jmp".to_string(),
+                        argument: ins.argument,
+                    },
+                    _ => {
+                        panic!();
+                    }
+                };
+                let mut new_instructions = vec![];
+                new_instructions.extend_from_slice(&instructions[..i]);
+                new_instructions.push(new_ins);
+                new_instructions.extend_from_slice(&instructions[i + 1..]);
+                if let (false, count) = has_loop(&new_instructions) {
+                    acc = count;
+                    break;
+                }
+            }
+        }
+        Err(_) => {
+            println!("Error reading file {}", filename);
+        }
+    };
+    acc
+}
+
+fn has_loop(instructions: &[Instruction]) -> (bool, isize) {
+    let total = instructions.len() as isize;
+    let mut executed_instructions: HashSet<isize> = HashSet::new();
+    let mut pointer: isize = 0;
+    let mut accumulator: isize = 0;
+
+    while pointer < total && !executed_instructions.contains(&pointer) {
+        executed_instructions.insert(pointer);
+
+        let instruction = &instructions[pointer as usize];
+        match &instruction.operation[..] {
+            "acc" => {
+                accumulator += instruction.argument;
+            }
+            "jmp" => {
+                pointer += instruction.argument;
+                continue;
+            }
+            _ => {}
+        };
+        pointer += 1;
+    }
+    (pointer < total, accumulator)
 }
 
 fn parse(line: &str) -> Instruction {
--- tests/day8_test.rs
+++ tests/day8_test.rs
@@ -1,14 +1,24 @@
 #[cfg(test)]
 mod day8_test {
-    use adventofcode_2020::day8::run;
+    use adventofcode_2020::day8::{run_1, run_2};
 
     #[test]
     fn part1_works_for_sample_input() {
-        assert_eq!(run("tests/inputs/day8_sample.txt"), 5);
+        assert_eq!(run_1("tests/inputs/day8_sample.txt"), 5);
     }
 
     #[test]
     fn part1_works_for_puzzle_input() {
-        assert_eq!(run("tests/inputs/day8_puzzle.txt"), 1528);
+        assert_eq!(run_1("tests/inputs/day8_puzzle.txt"), 1528);
+    }
+
+    #[test]
+    fn part2_works_for_sample_input() {
+        assert_eq!(run_2("tests/inputs/day8_sample.txt"), 8);
+    }
+
+    #[test]
+    fn part2_works_for_puzzle_input() {
+        assert_eq!(run_2("tests/inputs/day8_puzzle.txt"), 640);
     }
 }
