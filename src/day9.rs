ad445219b35eddff5a99e5442d302276fb2f8706
Tejas Bubane <tejasbubane@gmail.com>
2020-12-19 17:15:19 +0530

    Solve day-9 part-2

--- src/day9.rs
+++ src/day9.rs
@@ -1,14 +1,19 @@
 use crate::utils::read_lines;
 use itertools::Itertools;
 
-pub fn run(filename: &str, preamble_length: usize) -> usize {
+type Number = usize;
+type NumberList = Vec<usize>;
+
+// Finds first number in given list which does not satisfy the XMAS property
+// Also returns parsed NumberList to be used later
+pub fn run_1(filename: &str, preamble_length: usize) -> Option<(Number, NumberList)> {
     if let Ok(lines) = read_lines(filename) {
-        let lines: Vec<usize> = lines
+        let numbers: Vec<usize> = lines
             .map(|line| line.unwrap().parse::<usize>().unwrap())
             .collect();
-        let mut sum_candidates: Vec<usize> = lines[0..preamble_length].to_owned();
+        let mut sum_candidates: Vec<usize> = numbers[0..preamble_length].to_owned();
 
-        *lines[preamble_length..]
+        let result = numbers[preamble_length..]
             .iter()
             .find(|&&number| {
                 let not_xmas = !is_xmas(number, &sum_candidates);
@@ -19,10 +24,43 @@ pub fn run(filename: &str, preamble_length: usize) -> usize {
                 }
                 not_xmas
             })
-            .unwrap_or(&0)
+            .unwrap_or(&0);
+        Some((*result, numbers))
     } else {
-        0
+        None
     }
+}
+
+// Finds contiguous list of numbers which adds up to
+// the invalid-number which does not satisfy XMAS property (result of `run_1`)
+pub fn run_2(filename: &str, preamble_length: usize) -> Vec<usize> {
+    let (invalid_number, numbers) = run_1(filename, preamble_length).unwrap();
+    let mut set_size = 2; // atleast two numbers to sum - increase from there
+    let mut result: Vec<usize> = Vec::new();
+
+    while set_size < numbers.len() {
+        match chunks(&numbers, set_size)
+            .into_iter()
+            .find(|&s| s.iter().sum::<usize>() == invalid_number)
+        {
+            Some(set) => {
+                result = set.iter().copied().collect();
+                break;
+            }
+            None => set_size += 1,
+        }
+    }
+    result
+}
+
+// All consecutive slices of given length from the iterable `base`
+fn chunks<T>(base: &[T], size: usize) -> Vec<&[T]> {
+    let mut result: Vec<&[T]> = Vec::new();
+    let limit = base.len() - size;
+    for i in 0..limit {
+        result.push(&base[i..i + size]);
+    }
+    result
 }
 
 // Checks if given number satisfies the XMAS property
--- tests/day9_test.rs
+++ tests/day9_test.rs
@@ -1,14 +1,34 @@
 #[cfg(test)]
 mod day9_test {
-    use adventofcode_2020::day9::run;
+    use adventofcode_2020::day9::{run_1, run_2};
 
     #[test]
     fn part1_works_for_sample_input() {
-        assert_eq!(run("tests/inputs/day9_sample.txt", 5), 127);
+        assert_eq!(run_1("tests/inputs/day9_sample.txt", 5).unwrap().0, 127);
     }
 
     #[test]
     fn part1_works_for_puzzle_input() {
-        assert_eq!(run("tests/inputs/day9_puzzle.txt", 25), 400480901);
+        assert_eq!(
+            run_1("tests/inputs/day9_puzzle.txt", 25).unwrap().0,
+            400480901
+        );
+    }
+
+    #[test]
+    fn part2_works_for_sample_input() {
+        assert_eq!(
+            run_2("tests/inputs/day9_sample.txt", 5),
+            vec![15, 25, 47, 40]
+        );
+    }
+
+    #[test]
+    fn part2_works_for_puzzle_input() {
+        let set = run_2("tests/inputs/day9_puzzle.txt", 25);
+        let min = set.iter().min().unwrap();
+        let max = set.iter().max().unwrap();
+
+        assert_eq!(min + max, 67587168);
     }
 }
