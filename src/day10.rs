ed5bd7a531b733fb9f6df4455f4127470fc7c19f
Tejas Bubane <tejasbubane@gmail.com>
2020-12-19 22:08:29 +0530

    Solve day-10 part-2

--- src/day10.rs
+++ src/day10.rs
@@ -1,16 +1,9 @@
 use crate::utils::read_lines;
 use itertools::Itertools;
 
-pub fn run(filename: &str) -> (usize, usize) {
-    if let Ok(lines) = read_lines(filename) {
-        let mut joltages: Vec<usize> = lines
-            .map(|line| line.unwrap().parse::<usize>().unwrap())
-            .collect();
-        let source_joltage = 0;
-        let device_joltage = joltages.iter().max().unwrap() + 3;
-        joltages.push(source_joltage);
-        joltages.push(device_joltage);
-        let sorted: Vec<usize> = joltages.into_iter().sorted().collect();
+// part 1
+pub fn run_1(filename: &str) -> (usize, usize) {
+    if let Some(sorted) = joltages(filename) {
         let mut differences: Vec<usize> = Vec::new();
 
         for i in 0..(sorted.len() - 1) {
@@ -24,4 +17,62 @@ pub fn run(filename: &str) -> (usize, usize) {
 
 fn count<T: Eq>(list: &[T], el: T) -> usize {
     list.iter().filter(|&x| *x == el).count()
+}
+
+// reads file, parses numbers, collects them in vector
+// adds 0 and device (max + 3) to the list - sorts and then returns
+fn joltages(filename: &str) -> Option<Vec<usize>> {
+    if let Ok(lines) = read_lines(filename) {
+        let mut result: Vec<usize> = lines
+            .map(|line| line.unwrap().parse::<usize>().unwrap())
+            .collect();
+        let source_joltage = 0;
+        let device_joltage = result.iter().max().unwrap() + 3;
+        result.push(source_joltage);
+        result.push(device_joltage);
+        let sorted: Vec<usize> = result.into_iter().sorted().collect();
+        Some(sorted)
+    } else {
+        None
+    }
+}
+
+// part 2
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+pub fn run_2(filename: &str) -> usize {
+    if let Some(joltages) = joltages(filename) {
+        let set: HashSet<usize> = joltages.iter().cloned().collect();
+        let mut acc: HashMap<usize, usize> = HashMap::new();
+
+        recurse(0, &set, &mut acc) // sorted - so first element always zero
+    } else {
+        0
+    }
+}
+
+// count number of paths from given point onwards
+fn recurse(num: usize, set: &HashSet<usize>, cache: &mut HashMap<usize, usize>) -> usize {
+    if let Some(&val) = cache.get(&num) {
+        val
+    } else {
+        let mut result = 0;
+
+        if set.contains(&(num + 1)) {
+            result += recurse(num + 1, &set, cache)
+        }
+        if set.contains(&(num + 2)) {
+            result += recurse(num + 2, &set, cache)
+        }
+        if set.contains(&(num + 3)) {
+            result += recurse(num + 3, &set, cache)
+        }
+
+        if result == 0 {
+            result = 1
+        }
+        cache.insert(num, result);
+        result
+    }
 }
--- tests/day10_test.rs
+++ tests/day10_test.rs
@@ -1,17 +1,28 @@
 #[cfg(test)]
 mod day10_test {
-    use adventofcode_2020::day10::run;
+    use adventofcode_2020::day10::{run_1, run_2};
 
     #[test]
     fn part1_works_for_sample_input() {
-        assert_eq!(run("tests/inputs/day10_sample1.txt"), (7, 5));
-        assert_eq!(run("tests/inputs/day10_sample2.txt"), (22, 10));
+        assert_eq!(run_1("tests/inputs/day10_sample1.txt"), (7, 5));
+        assert_eq!(run_1("tests/inputs/day10_sample2.txt"), (22, 10));
     }
 
     #[test]
     fn part1_works_for_puzzle_input() {
-        let (diff_one, diff_three) = run("tests/inputs/day10_puzzle.txt");
+        let (diff_one, diff_three) = run_1("tests/inputs/day10_puzzle.txt");
 
         assert_eq!(diff_one * diff_three, 2450);
+    }
+
+    #[test]
+    fn part2_works_for_sample_input() {
+        assert_eq!(run_2("tests/inputs/day10_sample1.txt"), 8);
+        assert_eq!(run_2("tests/inputs/day10_sample2.txt"), 19208);
+    }
+
+    #[test]
+    fn part2_works_for_puzzle_input() {
+        assert_eq!(run_2("tests/inputs/day10_puzzle.txt"), 32396521357312);
     }
 }
