f47f8a6cf66d7e09657b7af64874885759412831
Tejas Bubane <tejasbubane@gmail.com>
2020-12-21 01:30:43 +0530

    Solve day-11 part-2

--- src/day11.rs
+++ src/day11.rs
@@ -25,16 +25,24 @@ impl fmt::Debug for Position {
     }
 }
 
-pub fn run(filename: &str) -> usize {
-    if let Ok(lines) = read_lines(filename) {
-        let lines: Vec<String> = lines.map(|line| line.unwrap()).collect();
-        let mut layout: Grid<Position> = grid![];
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum NeighbourType {
+    Immediate,
+    Visible,
+}
 
-        // Iterate over lines and populate the Grid
-        lines.iter().for_each(|line| parse_line(line, &mut layout));
+pub fn run_1(filename: &str) -> usize {
+    run(filename, NeighbourType::Immediate)
+}
 
+pub fn run_2(filename: &str) -> usize {
+    run(filename, NeighbourType::Visible)
+}
+
+fn run(filename: &str, neighbour_type: NeighbourType) -> usize {
+    if let Ok(mut layout) = parse_grid(filename) {
         loop {
-            let new_layout = mutate(&layout);
+            let new_layout = mutate(&layout, neighbour_type);
             if new_layout == layout {
                 break;
             }
@@ -49,16 +57,26 @@ pub fn run(filename: &str) -> usize {
 
 // Perform one round of mutation on the entire grid
 // Returns new grid - does NOT do in-place mutation
-fn mutate(layout: &Grid<Position>) -> Grid<Position> {
+fn mutate(layout: &Grid<Position>, neighbour_type: NeighbourType) -> Grid<Position> {
     let mut new_layout = layout.clone();
     let (row_count, col_count) = layout.size();
+    let occupy_limit = if neighbour_type == NeighbourType::Immediate {
+        4
+    } else {
+        5
+    };
 
     for i in 0..row_count {
         for j in 0..col_count {
-            let adjacent: Vec<Position> = neighbours(i, j, &layout);
+            let adjacent: Vec<Position> = if neighbour_type == NeighbourType::Immediate {
+                neighbours(i, j, &layout)
+            } else {
+                visible_neighbours(i, j, &layout)
+            };
+
             match layout[i][j] {
                 Position::Occupied => {
-                    if over_occupied(&adjacent) {
+                    if over_occupied(&adjacent, occupy_limit) {
                         new_layout[i][j] = Position::Empty;
                     }
                 }
@@ -74,6 +92,7 @@ fn mutate(layout: &Grid<Position>) -> Grid<Position> {
     new_layout
 }
 
+// Immediate neighbours for part-1
 fn neighbours<T: Clone + Copy>(row: usize, col: usize, layout: &Grid<T>) -> Vec<T> {
     let mut result: Vec<T> = Vec::new();
     let (row_limit, col_limit) = layout.size();
@@ -106,12 +125,144 @@ fn neighbours<T: Clone + Copy>(row: usize, col: usize, layout: &Grid<T>) -> Vec<
     result
 }
 
-fn over_occupied(list: &[Position]) -> bool {
-    list.iter().filter(|&&l| l == Position::Occupied).count() >= 4
+// Neighbours visible in all eight directions for part-2
+fn visible_neighbours(row: usize, col: usize, layout: &Grid<Position>) -> Vec<Position> {
+    let mut result: Vec<Position> = Vec::new();
+    let (row_limit, col_limit) = layout.size();
+    let mut r;
+    let mut c;
+
+    // 1. top left diagonal
+    r = row;
+    c = col;
+    while r > 0 && c > 0 {
+        r -= 1;
+        c -= 1;
+        let position = layout[r][c];
+        if is_seat(position) {
+            result.push(position);
+            break;
+        }
+    }
+
+    // 2. top up
+    r = row;
+    c = col;
+    while r > 0 {
+        r -= 1;
+        let position = layout[r][c];
+        if is_seat(position) {
+            result.push(position);
+            break;
+        }
+    }
+
+    // 3. top right diagonal
+    r = row;
+    c = col;
+    while r > 0 && c < (col_limit - 1) {
+        r -= 1;
+        c += 1;
+        let position = layout[r][c];
+        if is_seat(position) {
+            result.push(position);
+            break;
+        }
+    }
+
+    // 4. right
+    r = row;
+    c = col;
+    while c < (col_limit - 1) {
+        c += 1;
+        let position = layout[r][c];
+        if is_seat(position) {
+            result.push(position);
+            break;
+        }
+    }
+
+    // 5. right down diagonal
+    r = row;
+    c = col;
+    while r < (row_limit - 1) && c < (col_limit - 1) {
+        r += 1;
+        c += 1;
+        let position = layout[r][c];
+        if is_seat(position) {
+            result.push(position);
+            break;
+        }
+    }
+
+    // 6. down
+    r = row;
+    c = col;
+    while r < (row_limit - 1) {
+        r += 1;
+        let position = layout[r][c];
+        if is_seat(position) {
+            result.push(position);
+            break;
+        }
+    }
+
+    // 7. down left diagonal
+    r = row;
+    c = col;
+    while r < (row_limit - 1) && c > 0 {
+        r += 1;
+        c -= 1;
+        let position = layout[r][c];
+        if is_seat(position) {
+            result.push(position);
+            break;
+        }
+    }
+
+    // 8. left
+    r = row;
+    c = col;
+    while c > 0 {
+        c -= 1;
+        let position = layout[r][c];
+        if is_seat(position) {
+            result.push(position);
+            break;
+        }
+    }
+
+    result
+}
+
+// ---- Predicate helpers ----
+
+fn over_occupied(list: &[Position], limit: usize) -> bool {
+    list.iter().filter(|&&l| l == Position::Occupied).count() >= limit
 }
 
 fn no_occupied(list: &[Position]) -> bool {
     list.iter().all(|&l| l != Position::Occupied)
+}
+
+fn is_seat(position: Position) -> bool {
+    position != Position::Floor
+}
+
+// ---- Parsing ----
+
+fn parse_grid(filename: &str) -> Result<Grid<Position>, std::io::Error> {
+    match read_lines(filename) {
+        Ok(lines) => {
+            let lines: Vec<String> = lines.map(|line| line.unwrap()).collect();
+            let mut layout: Grid<Position> = grid![];
+
+            // Iterate over lines and populate the Grid
+            lines.iter().for_each(|line| parse_line(line, &mut layout));
+            Ok(layout)
+        }
+        Err(e) => Err(e),
+    }
 }
 
 fn parse_line(line: &str, layout: &mut Grid<Position>) {
--- tests/day11_test.rs
+++ tests/day11_test.rs
@@ -1,14 +1,24 @@
 #[cfg(test)]
 mod day11_test {
-    use adventofcode_2020::day11::run;
+    use adventofcode_2020::day11::{run_1, run_2};
 
     #[test]
     fn part1_works_for_sample_input() {
-        assert_eq!(run("tests/inputs/day11_sample.txt"), 37);
+        assert_eq!(run_1("tests/inputs/day11_sample.txt"), 37);
     }
 
     #[test]
     fn part1_works_for_puzzle_input() {
-        assert_eq!(run("tests/inputs/day11_puzzle.txt"), 2152);
+        assert_eq!(run_1("tests/inputs/day11_puzzle.txt"), 2152);
+    }
+
+    #[test]
+    fn part2_works_for_sample_input() {
+        assert_eq!(run_2("tests/inputs/day11_sample.txt"), 26);
+    }
+
+    #[test]
+    fn part2_works_for_puzzle_input() {
+        assert_eq!(run_2("tests/inputs/day11_puzzle.txt"), 1937);
     }
 }
